32414f1ae738481439d209fb5e07c91debe0ac17
newql <ljm@bupt.edu.cn>
2015-12-16 20:50:31 +0800

    change demo

--- mobile_yangfan/js/demo.js
+++ mobile_yangfan/js/demo.js
@@ -273,6 +273,7 @@ function addBill()
 			};
 
 			var storage = window.localStorage;
+			
 			var billItem = JSON.parse(storage.getItem("billItem")) || [];
 	        var billMoney = JSON.parse(storage.getItem("billMoney")) || {
 	            "income":0,//总收入
